198f2cbdc56a695d7a7f5e2403a08590c623e7b6
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-05-28 17:35:55 -0400

    Add files via upload
    Simple 'Website add function'  GUI first iteration

---
+++ interact.js
@@ -0,0 +1,12 @@
+//  Saba Kochadze
+//  May 28, 2023
+//  Lab 1 GUI Website
+
+
+function add ()
+{
+    var input1 = parseInt(document.getElementById("input_1").value, 10);
+    var input2 = parseInt(document.getElementById("input_2").value, 10);
+    var sum = input1 + input2;
+    window.alert( "Result is:  " + sum);
+}
