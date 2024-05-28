bf1b9fe11badd82fb41c4482484384cc41b0d619
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-05-28 17:32:59 -0400

    Simple Add/Sum Function
    This iteration of the 'website' is only capable of adding 2  numerical values.

---
+++ interact.js
@@ -0,0 +1,7 @@
+function add ()
+{
+    var input1 = parseInt(document.getElementById("input_1").value, 10);
+    var input2 = parseInt(document.getElementById("input_2").value, 10);
+    var sum = input1 + input2;
+    window.alert( "Result is:  " + sum);
+}
