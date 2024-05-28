50c7eff1cd3e5334b56bbdd9ecf9db224e2ce14f
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-05-28 17:34:58 -0400

    Delete interact.js

--- interact.js
+++
@@ -1,7 +0,0 @@
-function add ()
-{
-    var input1 = parseInt(document.getElementById("input_1").value, 10);
-    var input2 = parseInt(document.getElementById("input_2").value, 10);
-    var sum = input1 + input2;
-    window.alert( "Result is:  " + sum);
-}
