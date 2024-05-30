9901f38ea46bb7e5c754a2745da20c9917695bc3
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-05-30 15:23:09 -0400

    Update interact.js

--- interact.js
+++ interact.js
@@ -5,8 +5,53 @@
 
 function add ()
 {
+
+    document.getElementById("rad2").checked = false
+    document.getElementById("rad3").checked = false
+    document.getElementById("rad4").checked = false
     var input1 = parseInt(document.getElementById("input_1").value, 10);
     var input2 = parseInt(document.getElementById("input_2").value, 10);
-    var sum = input1 + input2;
-    window.alert( "Result is:  " + sum);
+    window.sum = input1 + input2;
+
+
+
 }
+function sub ()
+{
+    document.getElementById("rad1").checked = false
+    document.getElementById("rad3").checked = false
+    document.getElementById("rad4").checked = false
+    var input1 = parseInt(document.getElementById("input_1").value, 10);
+    var input2 = parseInt(document.getElementById("input_2").value, 10);
+    window.sum = input1 - input2;
+
+}
+function mult ()
+{
+    document.getElementById("rad1").checked = false
+    document.getElementById("rad2").checked = false
+    document.getElementById("rad4").checked = false
+    var input1 = parseInt(document.getElementById("input_1").value, 10);
+    var input2 = parseInt(document.getElementById("input_2").value, 10);
+    window.sum = input1 * input2;
+
+}
+function div ()
+{
+    document.getElementById("rad1").checked = false
+    document.getElementById("rad2").checked = false
+    document.getElementById("rad3").checked = false
+    var input1 = parseInt(document.getElementById("input_1").value, 10);
+    var input2 = parseInt(document.getElementById("input_2").value, 10);
+    window.sum = input1 / input2;
+
+}
+
+
+function oporation ()
+{
+    document.getElementById("rad1").checked = false;
+    var unt = document.getElementById("unit_input").value
+    window.alert("Result is: " + window.sum + " " + unt);
+}
+
