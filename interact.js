9c9b93180c121d91f9048158d8ff278dda6851ef
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-06-11 11:36:45 -0400

    Update interact.js
    added range slider

--- interact.js
+++ interact.js
@@ -2,6 +2,29 @@
 //  May 28, 2023
 //  Lab 1 GUI Website
 
+//Range Slider Update 6/11/2024
+
+
+//  Range Values
+
+function range_disp1(){
+
+    window.range1 = document.getElementById("slider1").value; // value scan works
+
+    window.paragrapgh = document.getElementById("range_value1");
+    paragrapgh.textContent = "Value: " + range1;
+
+}
+function range_disp2(){
+
+    window.range2 = document.getElementById("slider2").value; // value scan works
+
+    window.paragrapgh = document.getElementById("range_value2");
+    paragrapgh.textContent = "Value: " + range2;
+
+}
+
+
 
 function add ()
 {
@@ -12,7 +35,10 @@ function add ()
     var input1 = parseInt(document.getElementById("input_1").value, 10);
     var input2 = parseInt(document.getElementById("input_2").value, 10);
     window.sum = input1 + input2;
-
+//sliders
+    var slider1 = parseInt(document.getElementById("slider1").value, 10);
+    var slider2 = parseInt(document.getElementById("slider2").value, 10);
+    window.slidersum = slider1 + slider2;
 
 
 }
@@ -24,6 +50,10 @@ function sub ()
     var input1 = parseInt(document.getElementById("input_1").value, 10);
     var input2 = parseInt(document.getElementById("input_2").value, 10);
     window.sum = input1 - input2;
+//slider
+    var slider1 = parseInt(document.getElementById("slider1").value, 10);
+    var slider2 = parseInt(document.getElementById("slider2").value, 10);
+    window.slidersum = slider1 - slider2;
 
 }
 function mult ()
@@ -34,6 +64,10 @@ function mult ()
     var input1 = parseInt(document.getElementById("input_1").value, 10);
     var input2 = parseInt(document.getElementById("input_2").value, 10);
     window.sum = input1 * input2;
+//slider
+    var slider1 = parseInt(document.getElementById("slider1").value, 10);
+    var slider2 = parseInt(document.getElementById("slider2").value, 10);
+    window.slidersum = slider1 * slider2;    
 
 }
 function div ()
@@ -44,14 +78,40 @@ function div ()
     var input1 = parseInt(document.getElementById("input_1").value, 10);
     var input2 = parseInt(document.getElementById("input_2").value, 10);
     window.sum = input1 / input2;
-
+//slider
+    var slider1 = parseInt(document.getElementById("slider1").value, 10);
+    var slider2 = parseInt(document.getElementById("slider2").value, 10);
+    window.slidersum = slider1 / slider2;
 }
 
 
 function oporation ()
 {
-    document.getElementById("rad1").checked = false;
+
+
     var unt = document.getElementById("unit_input").value
-    window.alert("Result is: " + window.sum + " " + unt);
+
+    window.paragrapgh = document.getElementById("Calc-integer");
+    paragrapgh.textContent = "Result is: " + window.sum + " " + unt;
+
+    window.paragrapgh = document.getElementById("Calc-slider");
+    paragrapgh.textContent = "Slider Result is: " + window.slidersum + " " + unt;
+
+
 }
 
+
+
+
+// TO BE ADDED
+function   clear() 
+{
+    document.getElementById("rad1").checked = false
+    document.getElementById("rad2").checked = false
+    document.getElementById("rad3").checked = false
+    document.getElementById("rad4").checked = false
+    
+    document.getElementById('Calc-integer').innerHTML = '';
+
+    window.sum = 0;
+}
